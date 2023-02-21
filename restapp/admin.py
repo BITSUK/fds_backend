43ef9b078c263559a230df0d6fa3e6f72c27c389
BITSUK <2022cfse006@wilp.bits-pilani.ac.in>
2023-02-21 20:37:13 +0530

    First commit

---
+++ restapp/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Train, Station, Stop, Restaurant, RestMenu, Order, OrderItem, Payment
+
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('train_no', 'train_name')
+    
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('station_code', 'station_name')
+    
+class StopAdmin(admin.ModelAdmin):
+    list_display = ('train_no', 'stop_no', 'station_code', 'arrival_time', 'departure_time', 'halt', 'day')    
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('rest_id', 'rest_name', 'rest_address', 'rest_location_code', 'user_id', 
+                    'rest_type','rest_status', 'rest_rating')
+    
+class RestMenuAdmin(admin.ModelAdmin):
+    list_display = ('menu_id', 'rest_id', 'item_name', 'item_desc', 'item_type',
+                    'item_rate', 'item_discount', 'item_rating', 'item_status')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'rest_id', 'order_date', 'delivery_date', 'user_id',
+                     'contact_no', 'station_code', 'train_no', 
+                     'coach_no', 'seat_no', 'order_status', 'item_count', 'total_amount',
+                      'total_discount', 'tax', 'net_amount')
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('item_id', 'item_name', 'item_quantity' , 'item_rate' , 'item_discount' , 'order_id')
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('payment_id' , 'order_id', 'payment_date' , 'payment_amount' , 
+                    'payment_mode' , 'payment_ref','payment_status')
+
+admin.site.register(Train, TrainAdmin)
+admin.site.register(Station, StationAdmin)
+admin.site.register(Stop, StopAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(RestMenu, RestMenuAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(Payment, PaymentAdmin)
+
---
+++ restapp/models.py
@@ -0,0 +1,197 @@
+from django.db import models
+from django.urls import reverse
+
+# Train Model
+class Train(models.Model):    
+    train_no = models.CharField(max_length = 8)                   #Example: 12926
+    train_name = models.CharField(max_length = 40,)               #Example: Paschim SF Express
+
+    class Meta:
+        ordering = ['train_no']
+
+    def get_absolute_url(self):
+        return reverse('train-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.train_no  
+
+
+# Station Model
+class Station(models.Model):    
+    station_code = models.CharField(max_length = 5)                                     #Example: RTM  
+    station_name = models.CharField(max_length = 40)                                    #Example: Ratlam
+
+    class Meta:
+        ordering = ['station_code']
+
+    def get_absolute_url(self):
+        return reverse('station-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.station_code  
+    
+# Stop Model
+class Stop(models.Model): 
+    train_no = models.ForeignKey('Train', on_delete=models.SET_NULL, null=True)         #Example: 12926
+    stop_no = models.CharField(max_length = 2)                                          #Example: 2
+    station_code = models.ForeignKey('Station', on_delete=models.SET_NULL, null=True)   #Example: RTM
+    arrival_time = models.CharField(max_length = 5)                                     #Example: 07:20
+    departure_time = models.CharField(max_length = 5)                                   #Example: 07:25
+    halt = models.CharField(max_length = 4)                                             #Example: 5m
+    day = models.CharField(max_length = 4)                                              #Example: Day 1
+
+    class Meta:
+        ordering = ['train_no', 'stop_no']
+
+    def get_absolute_url(self):
+        return reverse('stop-detail', args=[str(self.id)])   
+        
+    def __str__(self):        
+        return self.stop_no
+    
+# Restaurant Model
+class Restaurant(models.Model):    
+    rest_id = models.CharField(max_length = 6)                                                  #Example: RID001
+    rest_name = models.CharField(max_length = 40,help_text='Restaurant Name')                   #Example: Haldiram
+    rest_address = models.CharField(max_length = 120)                                           #Example: 34, Street A, City B - 123456
+    rest_location_code = models.ForeignKey('Station', on_delete=models.SET_NULL, null=True)     #Example: RTM
+    user_id = models.CharField(max_length = 6)                                                  #Example: UID002
+    contact_person = models.CharField(max_length = 40)                                          #Example: Jatin Kumar
+    contact_no = models.CharField(max_length = 10)                                              #Example: 9812398123
+    REST_TYPE = (
+            ('0','Veg'),
+            ('1','Non-Veg'),
+        )                                            
+    rest_type = models.CharField(max_length = 1,choices=REST_TYPE,blank=False,default='0')      #Example: 0-Veg / 1-Non-veg
+    rest_rating = models.IntegerField(default=0)                                                #Example: 4
+
+    REST_STATUS = (
+            ('1', 'Open'),
+            ('0', 'Closed'),
+        )
+    rest_status = models.CharField(max_length = 1, 
+                                   choices=REST_STATUS, blank=False,default='1')         #Example: 1-Open , 0-Close
+
+    class Meta:
+        ordering = ['rest_name']
+
+    def get_absolute_url(self):
+        return reverse('restaurant-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.rest_id
+
+# Menu Model
+class RestMenu(models.Model):    
+    menu_id = models.CharField(max_length = 6)                                          #Example: MID001
+    rest_id = models.ForeignKey('Restaurant', on_delete=models.SET_NULL, null=True)     #Example: RID001
+    item_name = models.CharField(max_length = 40)                                       #Example: Thali
+    item_desc = models.CharField(max_length = 120)                                      #Example: 3 Chapati, Rice, Tadka Dal, Mix Veg, Salad, Sweets
+    ITEM_TYPE = (
+            ('0','Veg'),
+            ('1','Non-Veg'),
+        )
+    item_type = models.CharField(max_length = 1,choices=ITEM_TYPE,blank=False,default='0')   #Example: 0-Veg / 1-Non-veg
+    item_rate = models.DecimalField(max_digits=6, decimal_places=2)                     #Example: 25.00
+    item_discount = models.DecimalField(max_digits=6, decimal_places=2)                 #Example: -3:25
+    item_rating = models.IntegerField(default=0)                                        #Example: 4
+    MENU_ITEM_STATUS = (
+            ('1', 'Available'),
+            ('0', 'Unavailable'),
+        )
+    
+    item_status = models.CharField(max_length = 1,
+                                   choices=MENU_ITEM_STATUS, blank=False,default='1')    #Example: 1-Available ,0-Unavailable
+    
+    class Meta:
+        ordering = ['item_name']
+
+    def get_absolute_url(self):
+        return reverse('menu-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.menu_id 
+
+# Order Model
+class Order(models.Model):    
+    order_id = models.CharField(max_length = 6)                                         #Example: OID001
+    rest_id = models.ForeignKey('Restaurant', on_delete=models.SET_NULL, null=True)     #Example: RID001
+    order_date = models.DateField(null=True, blank=False)                               #Example: 02-02-2023
+    delivery_date = models.DateField(null=True, blank=False)                            #Example: 02-02-2023
+    user_id = models.CharField(max_length = 6)                                          #Example: UID001
+    contact_no = models.CharField(max_length = 10)                                      #Example: 9812398123
+    station_code = models.ForeignKey('Station', on_delete=models.SET_NULL, null=True)   #Example: RTM
+    train_no = models.ForeignKey('Train', on_delete=models.SET_NULL, null=True)         #Example: 12926
+    coach_no = models.CharField(max_length = 4)                                         #Example: B1
+    seat_no = models.IntegerField(default=0)                                            #Example: 22
+    ORDER_STATUS = (
+            ('0', 'Initial'),
+            ('1', 'Payment Done'), 
+            ('2', 'Payment Failed'),
+            ('3', 'Accepted'),
+            ('4', 'Rejected'),
+            ('5', 'Preparing Food'),
+            ('6', 'Order Ready'),
+            ('7', 'In Transit'),
+            ('8', 'Delivered'),
+            ('9', 'Delivery Failed'),
+            ('10', 'Cancelled'),
+        )
+    order_status = models.CharField(max_length = 2,choices=ORDER_STATUS, 
+                                    blank=False,default='0')                             #Example: 0,1,2,...
+    item_count = models.IntegerField(default=0)                                         #Example: 4
+    total_amount = models.DecimalField(max_digits=6, decimal_places=2)                  #Example: 300:50
+    total_discount = models.DecimalField(max_digits=6, decimal_places=2)                #Example: -40:00
+    tax = models.DecimalField(max_digits=6, decimal_places=2)                           #Example:  20:00
+    net_amount = models.DecimalField(max_digits=6, decimal_places=2)                    #Example: 280:50
+
+    class Meta:
+        ordering = ['order_id']
+
+    def get_absolute_url(self):
+        return reverse('order-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.order_id  
+
+# Item Model
+class OrderItem(models.Model):    
+    item_id = models.CharField(max_length = 6)                                      #Example: OTM001
+    item_name = models.CharField(max_length = 40)                                   #Example: Thali
+    item_quantity = models.IntegerField(default=0)                                  #Example: 2
+    item_rate = models.DecimalField(max_digits=6, decimal_places=2)                 #Example: 25.00
+    item_discount = models.DecimalField(max_digits=6, decimal_places=2)             #Example: -3:25
+    order_id = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)     #Example: OID001
+
+    class Meta:
+        ordering = ['item_id']
+
+    def get_absolute_url(self):
+        return reverse('item-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.item_id    
+
+# Payment Model
+class Payment(models.Model):    
+    payment_id = models.CharField(max_length = 6)                                   #Example: PID001
+    order_id = models.ForeignKey('Order', on_delete=models.SET_NULL, null=True)     #Example: OID001
+    payment_date = models.DateField(null=True, blank=False)                         #Example: 02-02-2023
+    payment_amount = models.DecimalField(max_digits=6, decimal_places=2)            #Example: 25.00
+    payment_mode = models.CharField(max_length = 10)                                #Example: Card
+    payment_ref = models.CharField(max_length = 20)                                 #Example: TRN20230220S0001
+    PAYMENT_STATUS = (
+            ('1', 'Success'),
+            ('0', 'Failed'),
+        )
+    payment_status = models.CharField(max_length = 1,choices=PAYMENT_STATUS, 
+                                      blank=False, default='0')                      #Example: 1-Success / 0-Failed 
+
+    class Meta:
+        ordering = ['payment_id']
+
+    def get_absolute_url(self):
+        return reverse('item-detail', args=[str(self.id)])   
+        
+    def __str__(self):
+        return self.payment_id   
