5ab98c4264178dca6474a6c54ab3537395b6f2df
BITSUK <2022cfse006@wilp.bits-pilani.ac.in>
2023-03-20 20:14:42 +0530

    a single commit after demo, we did not other change after submission

--- fds_backend/settings.py
+++ fds_backend/settings.py
@@ -124,7 +124,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15,
+    'PAGE_SIZE': 50,
 }
 
 # CORS_ORIGIN_ALLOW_ALL = True
